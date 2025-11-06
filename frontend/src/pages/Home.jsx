4fea2ccd4ee9ae75fc17a8e781938e79e263d385
VINODKUMAR36594 <v2325946@gmail.com>
2025-11-06 16:37:02 +0530

    Add GenderCollectionsSection and NewArrivals components to Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Hero from '../components/Layout/Hero'
+import GenderCollectionsSection from '../components/Products/GenderCollectionsSection'
+import NewArrivals from '../components/Products/NewArrivals'
 const Home = () => {
   return (
     <div>
         <Hero/>
+        <GenderCollectionsSection/>
+        <NewArrivals/>
       
     </div>
   )
