d98be38b8c46393b23768a34885b2dbb8307b99a
VINODKUMAR36594 <kumarvinod60700@gmail.com>
2025-12-25 12:52:24 +0530

    Implement FilterSlidebar and SortOptions components; update CollectionPage to integrate filtering and sorting functionality

--- frontend/src/components/Products/FilterSlidebar.jsx
+++ frontend/src/components/Products/FilterSlidebar.jsx
@@ -1,9 +1,186 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
 const FilterSlidebar = () => {
-  return (
-    <div>FilterSlidebar</div>
-  )
-}
+  const [searchParams] = useSearchParams();
 
-export default FilterSlidebar
+  const [filter, setFilter] = useState({
+    category: "",
+    gender: "",
+    size: [],
+    color: "",
+    material: [],
+    brand: [],
+    minPrice: 0,
+    maxPrice: 100,
+  });
+  const genders=["MEN","WOMEN"]
+  const sizes=["XS","S",'M',"l","xl","xxl"]
+  const [priceRange, setPriceRange] = useState([0, 100]);
+  const colors=[
+    "Red","Yellow","blue","green","black","brown","pink","white",
+  ]
+  // Array of clothing brands
+const brands = [
+  "Nike",
+  "Adidas",
+  "Zara",
+  "H&M",
+  "Levi's",
+  "Uniqlo",
+  "Gucci"
+];
+
+// Array of clothing materials
+const materials = [
+  "Cotton",
+  "Polyester",
+  "Denim",
+  "Wool",
+  "Silk",
+  "Linen",
+  "Leather"
+];
+
+
+  const categories = ["Top Wear", "Bottom Wear"];
+
+  useEffect(() => {
+    const params = Object.fromEntries([...searchParams]);
+
+    const min = params.minPrice ? Number(params.minPrice) : 0;
+    const max = params.maxPrice ? Number(params.maxPrice) : 100;
+
+    setFilter({
+      category: params.category || "",
+      gender: params.gender || "",
+      color: params.color || "",
+      size: params.size ? params.size.split(",") : [],
+      material: params.material ? params.material.split(",") : [],
+      brand: params.brand ? params.brand.split(",") : [],
+      minPrice: min,
+      maxPrice: max,
+    });
+
+    setPriceRange([min, max]);
+  }, [searchParams.toString()]);
+
+  return (
+    <div className="p-4">
+      <h3 className="text-xl font-medium text-gray-800 mb-4">
+        Filter
+      </h3>
+
+      {/* Category Filter */}
+      <div className="mb-6">
+        <label className="block text-gray-600 font-medium mb-2">
+          Category
+        </label>
+
+        {categories.map((category) => (
+          <div key={category} className="flex items-center mb-1">
+            <input
+              type="radio"
+              name="category"
+              value={category}
+              checked={filter.category === category}
+               onChange={(e) =>
+        setFilter((prev) => ({ ...prev, category: e.target.value }))
+      }
+              // readOnly
+              className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300"
+            />
+            <span className="text-gray-700">{category}</span>
+          </div>
+        ))}
+      </div>
+{/* gender Filter */}
+      <div className="mb-6">
+        <label className="block text-gray-600 font-medium mb-2">
+          Gender
+        </label>
+
+        {genders.map((gender) => (
+          <div key={gender} className="flex items-center mb-1">
+            <input
+              type="radio"
+              name="gender"
+              value={gender}
+              checked={filter.gender === gender}
+               onChange={(e) =>
+        setFilter((prev) => ({ ...prev, gender: e.target.value }))
+      }
+              // readOnly
+              className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300"
+            />
+            <span className="text-gray-700">{gender}</span>
+          </div>
+        ))}
+      </div>
+
+      {/* Color section */}
+      <div className="mb-6">
+        <label className="block text-gray-600 font-medium mb-2">Color</label>
+        <div className="flex flex-wrap gap-2">
+          {colors.map((color)=>{
+           return( <button key={color}
+            name="color"
+            className="w-8 h-8 rounded-full border border-gray-300 cursor-pointer transition hover:scale-105"
+            style={{backgroundColor:color.toLowerCase()}}></button>
+         ); })}
+        </div>
+      </div>
+         {/* size fliter */}
+         <div className="mb-6">
+          <label className="block text-gray-600 font-medium mb-2">Size</label>
+          {sizes.map((size)=>{
+            return(
+              <div key={size} className="flex items-center mb-1">
+                <input type="checkbox" name='size' className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300"/>
+                <span className="text-gray-700 ">{size.toUpperCase()}</span>
+              </div>
+            )
+          })}
+         </div>
+
+{/* materials */}
+
+<div className="mb-6">
+          <label className="block text-gray-600 font-medium mb-2">Size</label>
+          {materials.map((material)=>{
+            return(
+              <div key={material} className="flex items-center mb-1">
+                <input type="checkbox" name='size' className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300"/>
+                <span className="text-gray-700 ">{material.toUpperCase()}</span>
+              </div>
+            )
+          })}
+         </div>
+{/* brands */}
+<div className="mb-6">
+          <label className="block text-gray-600 font-medium mb-2">Size</label>
+          {brands.map((brand)=>{
+            return(
+              <div key={brand} className="flex items-center mb-1">
+                <input type="checkbox" name='size' className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300"/>
+                <span className="text-gray-700 ">{brand.toUpperCase()}</span>
+              </div>
+            )
+          })}
+         </div>
+
+         {/* {/price range fliter*  */}
+         <div className="mb-8 ">
+          <label className="block text-gray-600 font-medium mb-2">Price Range</label>
+          <input type="range" name="PriceRange" min={0} max={100} className="w-full h-2 bg-gray-300 rounded-lg appearance-none cursor-pointer" />
+          <div className="flex justify-between text-gray-600 mt-2">
+            <span >$0</span>
+            <span>${priceRange[1]}</span>
+           
+          </div>
+         </div>
+    </div>
+  );
+};
+
+export default FilterSlidebar;
--- frontend/src/pages/CollectionPage.jsx
+++ frontend/src/pages/CollectionPage.jsx
@@ -1,124 +1,77 @@
-import React,{useEffect,useRef,useState} from 'react'
-import {FaFilter} from 'react-icons/fa'
-import FilterSidebar from '../components/Products/FilterSlidebar'
+import React, { useEffect, useRef, useState } from 'react';
+import { FaFilter } from 'react-icons/fa';
+import FilterSidebar from '../components/Products/FilterSlidebar';
+import SortOptions from '../components/Products/SortOptions';
+import ProductGrid from '../components/Products/ProductGrid'
 const CollectionPage = () => {
-    const [products,setProducts]=useState([]);
-    const sidebarRef=useRef(null);
-    const [isSidebarOpen,setIsSidebarOpen]=useState(false);
+  const [products, setProducts] = useState([]);
+  const sidebarRef = useRef(null);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-    const toggleSideBar=()=>{
-        setIsSidebarOpen(!isSidebarOpen);
+  const toggleSideBar = () => {
+    setIsSidebarOpen(prev => !prev);
+    // setTimeout(10000)
+
+  };
+
+  const handleClickOutside = (e) => {
+    if (sidebarRef.current && !sidebarRef.current.contains(e.target)) {
+      setIsSidebarOpen(false);
+      
     }
-    // const handleClickOutside=(e)=>{
-    //     if(isSidebarRef.current && !sidebarRef.current.contains(e.target)) {
+  };
 
-    //     }
-    // }
-    useEffect(()=>{
-        //add event listenr for class
-        document.addEventListener("mousedown",handleClickOutside);
-    })
-    useEffect(()=>{
-        setTimeout(()=>{
-            const fetchProducts=[
-                 {
-        _id:1,
-        name:"product 1",
-        price:100,
-        images:[{
-            url:"https://picsum.photos/500/500?randome=1"
-        }],
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
+  useEffect(() => {
+    setTimeout(() => {
+      setProducts([
+        { _id: 1, name: "product 1", price: 100, images: [{ url: "https://picsum.photos/500/500?random=1" }] },
+        { _id: 2, name: "product 2", price: 100, images: [{ url: "https://picsum.photos/500/500?random=2" }] },
+        { _id: 3, name: "product 3", price: 100, images: [{ url: "https://picsum.photos/500/500?random=3" }] },
+        { _id: 4, name: "product 4", price: 100, images: [{ url: "https://picsum.photos/500/500?random=4" }] },
+        { _id: 5, name: "product 5", price: 100, images: [{ url: "https://picsum.photos/500/500?random=5" }] },
+        { _id: 6, name: "product 6", price: 100, images: [{ url: "https://picsum.photos/500/500?random=6" }] },
+        { _id: 7, name: "product 7", price: 100, images: [{ url: "https://picsum.photos/500/500?random=7" }] },
+        { _id: 8, name: "product 8", price: 100, images: [{ url: "https://picsum.photos/500/500?random=8" }] }
+      ]);
+    }, 1000);
+  }, []);
 
-    },
-    {
-        _id:2,
-        name:"product 2",
-        price:100,
-        images:[{
-            url:"https://picsum.photos/500/500?randome=2"
-        }],
-
-
-    },
-    {
-        _id:3,
-        name:"product 3",
-        price:100,
-        images:[{
-            url:"https://picsum.photos/500/500?randome=3"
-        }],
-
-
-    },
-    {
-        _id:4,
-        name:"product 4",
-        price:100,
-        images:[{
-            url:"https://picsum.photos/500/500?randome=4"
-        }],
-
-
-    },
-     {
-        _id:1,
-        name:"product 5",
-        price:100,
-        images:[{
-            url:"https://picsum.photos/500/500?randome=5"
-        }],
-
-
-    },
-    {
-        _id:2,
-        name:"product 6",
-        price:100,
-        images:[{
-            url:"https://picsum.photos/500/500?randome=6"
-        }],
-
-
-    },
-    {
-        _id:3,
-        name:"product 7",
-        price:100,
-        images:[{
-            url:"https://picsum.photos/500/500?randome=7"
-        }],
-
-
-    },
-    {
-        _id:4,
-        name:"product 8",
-        price:100,
-        images:[{
-            url:"https://picsum.photos/500/500?random=8"
-        }],
-
-
-    },
-
-            ];
-            setProducts(fetchProducts);
-        },1000)
-    },[])
   return (
     <div className='flex flex-col lg:flex-row'>
-        {/* MObile  FLiter button */}
-        <button className='lg:hidden border p-2 flex justify-center items-center'>
-            <FaFilter  className='mr-2'/>
-        </button>
-        {/* filter Sidebar */}
-        <div ref={sidebarRef}>
-        <FilterSidebar/>
-        </div>
+      {/* Mobile Filter Button */}
+      <button
+        onClick={toggleSideBar}
+        className='lg:hidden border p-2 flex justify-center items-center'
+      >
+        <FaFilter className='mr-2' />
+        Filter
+      </button>
 
+      {/* Filter Sidebar */}
+      <div
+        ref={sidebarRef}
+        className={`${
+          isSidebarOpen ? "translate-x-0" : "-translate-x-full"
+        } fixed inset-y-0 z-50 left-0 w-64 bg-white overflow-y-auto transition-transform duration-300 lg:static lg:translate-x-0`}
+      >
+        <FilterSidebar />
+      </div>
+      <div className='flex-grow p-4 '>
+        <h2 className='text-2xl uppercase mb-4 '>All Collection</h2>
+        {/* sort option */}
+        <SortOptions/>
+        {/* Proudct Grid */}
+        <ProductGrid products={products}/>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CollectionPage
+export default CollectionPage;
